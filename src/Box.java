8361c38e9a1c00c6352783047754ac0cf7c066a1
k3rr13n <bastian.kerrien@etu.univ-orleans.fr>
2025-03-06 09:20:19 +0100

    fin Td

--- src/Box.java
+++ src/Box.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 
-class Box{
+public class Box{
+    boolean etatBoite;
+    int capacite;
     ArrayList<Thing> contents = new ArrayList<Thing>();
+    public Box(){
+        this.capacite = -1;
+    }
     public void add(Thing truc){
         this.contents.add(truc);
     }
@@ -13,5 +18,45 @@ class Box{
             if (this.contents.get(i) == truc)
                 this.contents.remove(i);
         }
+    }
+    public boolean isOpen(){
+        return this.etatBoite;
+    }
+    public void close(){
+        this.etatBoite = false;
+    }
+    public void open(){
+        this.etatBoite = true;
+    }
+    public String actionLook(){
+        if (etatBoite == true){
+            return "la boite contient: ceci, cela";
+        }
+        return "la boite est fermee";
+    }
+    public void setCapacity(int volume){
+        this.capacite = volume;
+    }
+    public int capacity(){
+        return this.capacite;
+    }
+    public boolean hasRoomFor(Thing truc){
+        if (this.capacite < 0 && this.capacite == -1) {return true;}   
+        if (this.capacite - truc.volume >= 0) {return true;} 
+        return false;
+    }
+    public void actionAdd(Thing truc){
+        if (this.hasRoomFor(truc) == true && this.isOpen() == true){
+            this.capacite -= truc.volume;
+            this.add(truc);
+        }
+    }
+    public Thing find(String nomTruc){
+        for (Thing truc : this.contents){
+            if (truc.hasName(nomTruc) == true){
+                return truc;
+            }
+        }
+        return null;
     }
 }
--- src/Thing.java
+++ src/Thing.java
@@ -1,6 +1,29 @@
-class Thing{
+public class Thing{
     String name;
+    int volume;
     public Thing(String name){
         this.name = name;
+    }
+    public Thing(int volume){
+        this.volume = volume;
+    }
+    public Thing(String name, int volume){
+        this.name = name;
+        this.volume = volume;
+    }
+    public int volume(){
+        return this.volume;
+    }
+    public void setName(String nom){
+        this.name = nom;
+    }
+    public boolean hasName(String nom){
+        if (this.name == nom) {return true;}
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "Nom " + this.name + ", Volume " + this.volume;
     }
 }
